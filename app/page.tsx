6f3577f6f8f0d9d985e8c6ce02c4220c0af93b8a
Viviane <vivianeaguiarc@outlook.com>
2025-01-30 12:01:30 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Header from "./_components/Header";
+import Search from "./_components/Search";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
