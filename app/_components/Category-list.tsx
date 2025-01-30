bd8ea7edebbb2c72b523fe055d12967492abdb11
Viviane <vivianeaguiarc@outlook.com>
2025-01-30 12:09:20 -0300

    feat: add prisma file

---
+++ app/_components/Category-list.tsx
@@ -0,0 +1,11 @@
+const CategoryList = () => {
+  // pegar as categorias do banco de dados
+  // renderizar um item para cada categoria
+  return (
+    <div>
+      <h1>Dados</h1>
+    </div>
+  );
+};
+
+export default CategoryList;
