8052bdd339b984f71ac4089205d06abdd7ccf97d
Viviane <vivianeaguiarc@outlook.com>
2025-02-02 08:40:01 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/Category-list";
 import Header from "./_components/Header";
 import Search from "./_components/Search";
@@ -11,6 +12,17 @@ export default function Home() {
       </div>
       <div className="px-5 py-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt={"Até 30% de desconto"}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full"
+          quality={100}
+        />
       </div>
     </>
   );
